d6180c47418596c2790a21623ad73a268462f5de
DarkReaper009 <gabias.akaly@gmail.com>
2026-02-07 01:28:00 +0200

    chore: update cors and vercel file

--- src/server.js
+++ src/server.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT || 5000;
 app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://tranzitluc.com", // Replace with your actual frontend URL
+  }),
+);
 
 // Routes
 app.use("/api/contact", contact);
