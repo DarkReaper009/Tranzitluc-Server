c979701c4771e736d2202896d53b1ba7c4d62dd3
DarkReaper009 <gabias.akaly@gmail.com>
2026-02-07 02:44:30 +0200

    chore: url encoded setting

--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
     origin: "https://www.tranzitluc.com", // Replace with your actual frontend URL
